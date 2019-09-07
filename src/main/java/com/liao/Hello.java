306ff2208b020a7cea6930c2dbb2535db61936b8
Liao <liaoyu1313@gmail.com>
2019-09-07 17:45:34 +0800

    Initial commit

---
+++ src/main/java/com/liao/Hello.java
@@ -0,0 +1,10 @@
+package com.liao;
+
+public class Hello {
+    public static void main(String[] args){
+//        System.out.println("Hello World");
+//        new Person().hello();
+        Person p = new Person();
+        p.hello();
+    }
+}
