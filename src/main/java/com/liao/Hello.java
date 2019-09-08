fea897d63d9cd3a123875093fb873dd15b4570bf
Liao <liaoyu1313@gmail.com>
2019-09-08 18:30:09 +0800

    Java and Kotlin variable, data type

--- src/main/java/com/liao/Hello.java
+++ src/main/java/com/liao/Hello.java
@@ -6,5 +6,14 @@ public class Hello {
 //        new Person().hello();
         Person p = new Person();
         p.hello();
+        int age = 19;
+        Integer age2 = 19;
+        char c = '我';
+        Character c2 = 'A';
+        byte b = 120;
+        float weight = 66.5f;
+        float height = 1.7f;
+        boolean enroll = false;
+        String name = "Tom";
     }
 }
